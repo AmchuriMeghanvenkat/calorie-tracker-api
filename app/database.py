43f82708fc2ecc117a9091d0f6622fb4c81bb3d7
Amchuri Meghan venkat <anu.mv.1299@gmail.com>
2026-02-28 23:31:09 +0530

    use environment DATABASE_URL

--- app/database.py
+++ app/database.py
@@ -1,25 +1,25 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,declarative_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-    
-DATABASE_URL="mysql+pymysql://root:Meghan%40429@127.0.0.1:3306/calorie_db"
-
-def get_db():
-    db=SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-engine=create_engine(
+engine = create_engine(
     DATABASE_URL,
     echo=True
 )
-SessionLocal=sessionmaker(
+
+SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-Base=declarative_base()
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
